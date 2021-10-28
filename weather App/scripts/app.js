c234767fa29790d47566b41ce6fbafb768440386
yonatanaduk <84029275+yonatanaduk@users.noreply.github.com>
2021-10-28 15:12:33 +0300

    Add files via upload

--- weather App/scripts/app.js
+++ weather App/scripts/app.js
@@ -1,14 +1,63 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card')
+const details = document.querySelector('.details')
+const time = document.querySelector('img.time')
+const icon = document.querySelector('.icon img')
+
+const updateUI = (data) => {
+
+
+
+        //one way to do this:
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
+
+    //second way- destructure properties
+    const {cityDets , weather} = data;
+    console.log(data)
+
+    //update details template 
+ details.innerHTML = `
+      <h5 class="my-5">${cityDets.EnglishName}</h5>
+      <div class="my-3">${weather.WeatherText}</div>
+      <div class="display-4 mt-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+      </div>
+ `;
+
+ // update the night/day & icon images
+let iconSrc = `img/icons/${weather.WeatherIcon}.svg`
+icon.setAttribute('src' , iconSrc)
+
+
+      // one way to do this:
+// let timeSrc = null;
+// if(weather.IsDayTime){
+//     timeSrc = 'img/day.svg';
+// }else{
+//     timeSrc = 'img/night.svg';
+// }
+
+       //second wat to do this:
+       let timeSrc = weather.IsDayTime ? 'img/day.svg': 'img/night.svg'
+time.setAttribute('src' , timeSrc)
+
+
+// remove the d-none class if present
+if(card.classList.contains('d-none')) {
+    card.classList.remove('d-none')
+}
+};
+
+
 
 const updateCity = async (city) => {
     
     const cityDets = await getCity(city);
     const weather = await getWeather(cityDets.Key)
 
-    return{
-        cityDets:cityDets ,
-        weather:weather
-    };
+    return { cityDets , weather};
 } ;
 
 
@@ -22,6 +71,11 @@ cityForm.reset()
 
 //update the ui with new city
 updateCity(city)
-.then(data => console.log(data))
+.then(data => updateUI(data))
 .catch(err => console.log(err))
 })
+
+
+
+
+
